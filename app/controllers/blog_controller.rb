43fbb6273da1baf57667a4b02e74955a2e3ab34a
duonglan <duong.thi.lan@framgia.com>
2013-12-03 17:32:34 +0700

    view project

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,6 +2,8 @@ class BlogController < ApplicationController
   def home
     if signed_in?
       redirect_to user_microposts_path(current_user)
+    else
+      redirecto_to signin_path
     end
  
  end
