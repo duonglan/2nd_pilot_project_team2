97b9621996ed21c41cd4ea27763a654c9a156c98
kynm <kynm@kynm.(none)>
2013-12-05 00:10:45 +0700

    friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
   has_many :joined_groups, class_name:"GroupMember",  dependent: :destroy
   has_many :microposts, dependent: :destroy
   has_many :comments, through: :microposts, dependent: :destroy
+  has_many :friends, :through => :friendships, :conditions => "status = 'accepted'"
+  has_many :requested_friends, :through => :friendships, :source => :friend, :conditions => "status = 'requested'", :order => :created_at
+  has_many :pending_friends, :through => :friendships, :source => :friend, :conditions => "status = 'pending'", :order => :created_at
+  has_many :friendships, :dependent => :destroy
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 } 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203061246) do
+ActiveRecord::Schema.define(version: 20131204120958) do
 
   create_table "comments", force: true do |t|
     t.string   "content"
     t.integer  "micropost_id"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "friendships", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.string   "status"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
